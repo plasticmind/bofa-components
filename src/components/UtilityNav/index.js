baa5d96860a6df2646e309c3cfd218ef29ac958c
Jesse Gardner <jesse@plasticmind.com>
2023-10-19 22:23:57 -0400

    Added UtlityNav class

---
+++ src/components/UtilityNav/index.js
@@ -0,0 +1,18 @@
+import './UtilityNav.css';
+
+function UtilityNav() {
+  return (
+    <nav className="UtilityNav" role="navigation" aria-label="Top Menu">
+      <ul className="UtilityNav-menu" role="menu">
+        <li className="UtilityNav-item" role="menuitem"><a href="https://example.com">Personal</a></li>
+        <li className="UtilityNav-item" role="menuitem"><a href="https://business.example.com">Small Business</a></li>
+        <li className="UtilityNav-item" role="menuitem"><a href="https://wealth.example.com">Wealth Management</a></li>
+        <li className="UtilityNav-item" role="menuitem"><a href="https://business.example.com">Businesses & Institutions</a></li>
+        <li className="UtilityNav-item" role="menuitem"><a href="https://security.example.com">Security</a></li>
+        <li className="UtilityNav-item" role="menuitem"><a href="https://about.example.com">About</a></li>
+      </ul>
+    </nav>
+  );
+}
+
+export default UtilityNav;
